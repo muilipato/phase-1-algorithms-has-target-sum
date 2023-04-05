47250a8da59472b9c10fc157a9776c40fb99eac3
Patrick Maweu <muilipato@gmail.com>
2023-04-05 14:32:01 +0300

    modified my comments to be clearer

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Write your algorithm here
 function hasTargetSum(array, target) {
   for(let i=0; i<array.length-1; i++){
     for(let j=i+1;j<array.length; j++){
@@ -7,19 +8,32 @@ function hasTargetSum(array, target) {
     }
   }
   return false;
-  // Write your algorithm here
+  
+  
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Since the function has 2 loops, the time complexity is O(n*n) i.e O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  Iterate over the numbers in the array 
+    Iterate over the remaining numbers in the array
+      Check if the sum of the current number and any other number remaining in the array adds up to the target
+        If the sum = target then return true otherwise the function returns false
+
 */
 
 /*
   Add written explanation of your solution here
+  I want to go through the entire array for every number comparing with the rest of the numbers in the array
+  I could write a function with 2 for loops, one for looping over the numbers in the array 
+  and the other for iterating over the rest of the numbers in the array checking whether for each number, 
+  the sum of any number paired with it will match our target. 
+  If our sum equals our target then our function returns true otherwise it returns false.
+
 */
 
 // You can run `node index.js` to view these console logs
